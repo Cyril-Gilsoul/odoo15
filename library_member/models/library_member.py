bc5f51fbdab0cfa503b2498cd185e4ab14b53eff
Cyril-Gilsoul <cyril.gilsoul@apprenant.ifapme.be>
2023-02-20 09:28:23 +0100

    Git create

---
+++ library_member/models/library_member.py
@@ -0,0 +1,10 @@
+from odoo import fields, models
+
+
+class Member(models.Model):
+    _name = "library.member"
+    _description = "Library Member"
+    _inherits = {"res.partner": "partner_id"}
+    _inherit = ["mail.thread", "mail.activity.mixin"]
+    card_number = fields.Char()
+    partner_id = fields.Many2one("res.partner", ondelete="cascade", required=True)
