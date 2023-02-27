8109af94f1469a960de9b471f4aa0bfe4f2b3fb9
Cyril-Gilsoul <cyril.gilsoul@apprenant.ifapme.be>
2023-02-27 15:46:05 +0100

    Create wizard, view, class

--- library_checkout/__manifest__.py
+++ library_checkout/__manifest__.py
@@ -5,6 +5,7 @@
     "depends": ["library_member", "mail"],
     "data": [
         "security/ir.model.access.csv",
+        "wizard/checkout_mass_message_wizard_view.xml",
         "views/library_menu.xml",
         "views/checkout_view.xml",
         "data/library_checkout_stage.xml",
---
+++ library_checkout/wizard/checkout_mass_message.py
@@ -0,0 +1,31 @@
+from odoo import api, exceptions, fields, models
+
+
+class CheckoutMassMessage(models.TransientModel):
+    _name = "library.checkout.massmessage"
+    _description = "Send Message to Borrowers"
+
+    checkout_ids = fields.Many2many(
+        "library.checkout",
+        string="Checkouts",
+    )
+    message_subject = fields.Char()
+    message_body = fields.Html()
+
+    def button_send(self):
+        self.ensure_one()
+        if not self.checkout_ids:
+            raise exceptions.UserError(
+                "No Checkouts were selected."
+            )
+        if not self.message_body:
+            raise exceptions.UserError(
+                "A message body is required"
+            )
+        for checkout in self.checkout_ids:
+            checkout.message_post(
+                body=self.message_body,
+                subject=self.message_subject,
+                subtype='mail.mt_comment',
+            )
+        return True
