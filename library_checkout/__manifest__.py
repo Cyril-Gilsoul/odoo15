35c32b3d2ea3cc4f7992d6c2d2087ecbac377595
Cyril-Gilsoul <cyril.gilsoul@apprenant.ifapme.be>
2023-02-27 12:02:05 +0100

    add mail dependencies

--- library_checkout/__manifest__.py
+++ library_checkout/__manifest__.py
@@ -2,7 +2,7 @@
     "name": "Library Book Checkout",
     "description": "Members can borrow books from the library.",
     "author": "Daniel Reis",
-    "depends": ["library_member"],
+    "depends": ["library_member", "mail"],
     "data": [
         "security/ir.model.access.csv",
         "views/library_menu.xml",
