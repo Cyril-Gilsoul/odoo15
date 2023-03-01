c5bf62cdd97646f14cfd7ed98e95ae9e83c786d7
Cyril-Gilsoul <cyril.gilsoul@apprenant.ifapme.be>
2023-03-01 11:53:27 +0100

    add kanban view

--- library_checkout/__manifest__.py
+++ library_checkout/__manifest__.py
@@ -5,9 +5,10 @@
     "depends": ["library_member", "mail"],
     "data": [
         "security/ir.model.access.csv",
-        "wizard/checkout_mass_message_wizard_view.xml",
         "views/library_menu.xml",
         "views/checkout_view.xml",
+        "views/checkout_kanban_view.xml",
+        "wizard/checkout_mass_message_wizard_view.xml",
         "data/library_checkout_stage.xml",
-],
+    ],
 }
